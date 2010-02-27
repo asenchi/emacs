fac80099c42f8400b77e3b2c35d484ee21ae4c03
Curt Micol <asenchi@asenchi.com>
2010-02-27 02:00:04 -0500

    added in minimap.

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -55,7 +55,8 @@
       (setq load-path (cons "/usr/local/lib/erlang/lib/tools-2.6.5/emacs" load-path))
       (setq erlang-root-dir "/usr/local/lib/erlang")
       (setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
-      (require 'erlang-start)))
+      (require 'erlang-start)
+      (require 'minimap)))
 
 ;; customizations
 (require 'global)
