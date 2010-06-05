0d247053de7b31d9cb690c2f2a3a1332817a2fbb
Curt Micol <asenchi@asenchi.com>
2010-06-04 20:41:51 -0400

    init cleanup and removed cruft from vendor

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -17,11 +17,6 @@
   (cd *vendor-path*)
   (normal-top-level-add-subdirs-to-load-path))
 
-;; Looks for configurations named after system hostname.
-(setq system-specific-config
-      (concat *emacs-path* (car (split-string system-name "\\.")) ".el"))
-(if (file-exists-p system-specific-config) (load system-specific-config))
-
 (setq custom-file (concat *emacs-path* "/custom.el"))
 (load custom-file 'noerror)
 
@@ -33,7 +28,6 @@
 (require 'egg)
 (require 'color-theme)
 (require 'puppet-mode)
-;(require 'js2-mode)
 (autoload #'espresso-mode "espresso" "Start espresso-mode" t)
 
 (require 'ido)
@@ -45,23 +39,18 @@
 (setq uniquify-buffer-name-style 'forward)
 (require 'python-mode)
 (setq py-python-command-args '( "-colors" "Linux"))
-(require 'coffee-mode)
 (require 'tabbar)
 (require 'mustache-mode)
+(require 'rvm)
+(rvm-use-default)
 
 ;; Load textmate and org-babel on Macbook.
 (if (eq system-type 'darwin)
-    '((require 'textmate)
-      (textmate-mode)
-      (require 'org-jekyll)
-      (require 'org-babel-init)
+    '((require 'org-babel-init)
       (require 'org-babel-python)
       (org-babel-load-library-of-babel)
-      (setq load-path (cons "/usr/local/lib/erlang/lib/tools-2.6.5/emacs" load-path))
-      (setq erlang-root-dir "/usr/local/lib/erlang")
-      (setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
-      (require 'erlang-start)
-      (require 'minimap)))
+      (setq load-path
+            (cons "/usr/local/lib/erlang/lib/tools-2.6.5/emacs" load-path))))
 
 ;; customizations
 (require 'global)
@@ -77,5 +66,4 @@
         yas/dropdown-prompt))
 
 (server-start)
-
 (provide 'init)
